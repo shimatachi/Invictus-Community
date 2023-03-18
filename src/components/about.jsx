b9f8fc9baa7c55c5299a06d65400a94e103c57f1
shimatachi <thoriqalbarp1@gmail.com>
2023-03-19 03:46:18 +0700

    75%

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -2,45 +2,77 @@ import React from "react";
 
 export const About = (props) => {
   return (
-    <section class="page-section" id="services">
-        <div class="row row-cols-1 row-cols-md-3 g-4">
+    <section class="page-section bg-mlx" id="services">
+        <div class="row row-cols-1 row-cols-md-3 g-5 px-3 mt-100">
         <div class="col">
-            <div class="card h-100">
+            <div class="card h-100 bg-dark-blue">
             <img src="../img/misc/card1.png" class="card-img-top" alt="..."/>
-            <div class="card-body">
-                <h3 class="card-title">LEGIO - ORDO</h3>
-                <h5 class="card-title">Blue Protocol</h5>
-                <p class="card-text">Nullam quam massa, fringilla quis tortor eu, aliquet faucibus leo. Proin mollis erat congue, interdum ipsum sed, venenatis ante. Fusce non ultrices urna. Proin imperdiet quam diam, vel hendrerit mauris tempor et. Curabitur ut turpis sit amet mi blandit lobortis. Ut bibendum mauris non vehicula pellentesque. Ut ultricies feugiat neque nec ultricies. Fusce vulputate tortor fringilla fermentum congue.</p>
-                <small class="text-muted">Learn more</small>
-                <hr class="divider divider-light" />
-            </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card h-100">
-            <img src="../img/misc/card1.png" class="card-img-top" alt="..."/>
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-            </div>
-            <div class="card-footer">
-                <small class="text-muted">Last updated 3 mins ago</small>
+            <div class="card-body bg-dark-blue">
+                <h3 class="card-title text-white">LEGIO - ORDO</h3>
+                <h5 class="card-title text-white">Blue Protocol</h5>
+                <p class="card-text text-white">Nullam quam massa, fringilla quis tortor eu, aliquet faucibus leo. Proin mollis erat congue, interdum ipsum sed, venenatis ante. Fusce non ultrices urna. Proin imperdiet quam diam, vel hendrerit mauris tempor et. Curabitur ut turpis sit amet mi blandit lobortis. Ut bibendum mauris non vehicula pellentesque. Ut ultricies feugiat neque nec ultricies. Fusce vulputate tortor fringilla fermentum congue.</p>
+                <h5 class="card-title text-white">Learn More</h5>
+                <hr class="divider" />
             </div>
             </div>
         </div>
         <div class="col">
-            <div class="card h-100">
+            <div class="card h-100 bg-dark-blue">
             <img src="../img/misc/card1.png" class="card-img-top" alt="..."/>
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+            <div class="card-body bg-dark-blue">
+                <h3 class="card-title text-white">LEGIO - ORDO</h3>
+                <h5 class="card-title text-white">Blue Protocol</h5>
+                <p class="card-text text-white">Nullam quam massa, fringilla quis tortor eu, aliquet faucibus leo. Proin mollis erat congue, interdum ipsum sed, venenatis ante. Fusce non ultrices urna. Proin imperdiet quam diam, vel hendrerit mauris tempor et. Curabitur ut turpis sit amet mi blandit lobortis. Ut bibendum mauris non vehicula pellentesque. Ut ultricies feugiat neque nec ultricies. Fusce vulputate tortor fringilla fermentum congue.</p>
+                <h5 class="card-title text-white">Learn More</h5>
+                <hr class="divider" />
             </div>
-            <div class="card-footer">
-                <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100 bg-dark-blue">
+            <img src="../img/misc/card1.png" class="card-img-top" alt="..."/>
+            <div class="card-body bg-dark-blue">
+                <h3 class="card-title text-white">LEGIO - ORDO</h3>
+                <h5 class="card-title text-white">Blue Protocol</h5>
+                <p class="card-text text-white">Nullam quam massa, fringilla quis tortor eu, aliquet faucibus leo. Proin mollis erat congue, interdum ipsum sed, venenatis ante. Fusce non ultrices urna. Proin imperdiet quam diam, vel hendrerit mauris tempor et. Curabitur ut turpis sit amet mi blandit lobortis. Ut bibendum mauris non vehicula pellentesque. Ut ultricies feugiat neque nec ultricies. Fusce vulputate tortor fringilla fermentum congue.</p>
+                <h5 class="card-title text-white">Learn More</h5>
+                <hr class="divider" />
             </div>
             </div>
         </div>
         </div>
+
+        {/* Sponsor */}
+        <div class="container px-4 px-lg-5 mt-100">
+                <div class="row gx-4 gx-lg-5">
+                    <div class="col-lg-20 col-md-6 text-center">
+                        <div class="mt-5">
+                            <img src="../img/sponsorship/nvidiaw1.png"  width="120" height="120" alt="..."/>
+                        </div>
+                    </div>
+                    <div class="col-lg-20 col-md-6 text-center">
+                        <div class="mt-5">
+                            <img src="../img/sponsorship/logigw1.png"  width="120" height="120" alt="..."/>
+                        </div>
+                    </div>
+                    <div class="col-lg-20 col-md-6 text-center">
+                        <div class="mt-5">
+                            <img src="../img/sponsorship/BPIicon1.png"  width="120" height="120" alt="..."/>
+                        </div>
+                    </div>
+                    <div class="col-lg-20 col-md-6 text-center">
+                        <div class="mt-5">
+                            <img src="../img/sponsorship/kominfo1.png"  width="120" height="120" alt="..."/>
+                        </div>
+                    </div>
+                    <div class="col-lg-20 col-md-6 text-center">
+                        <div class="mt-5">
+                            <img src="../img/sponsorship/bswb1.png"   width="120" height="120" alt="..."/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
     </section>
   );
 };
